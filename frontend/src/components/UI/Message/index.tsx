86b7b460f2385f0facff83521edea9f797da60a4
fatfirebird <fatfirebird@gmail.com>
2021-01-17 16:11:32 +0500

    add autoscroll, add send time, add usernames

--- frontend/src/components/UI/Message/index.tsx
+++ frontend/src/components/UI/Message/index.tsx
@@ -12,15 +12,26 @@ interface IMessage extends BoxProps {
 const Message: React.FC<IMessage> = (props) => {
   const { isMine, children, username } = props
 
+  const currentDate = new Date()
+  const currentTime = `${currentDate.getHours()}:${currentDate.getMinutes()}`
+
   return(
     <Box className="message" display="flex" alignItems="center" justifyContent={isMine && 'flex-end'}>
       <Box marginRight="16px">
         { !isMine && <UserAvatar username={username} /> }
       </Box>
-      <Box className="message__text-container" padding="12px">
-        <div className={`message__text-decoration ${isMine && 'message__text-decoration--hidden'}`}></div>
-        <Typography className="message__text">{children}</Typography>
-      </Box>
+      <Box>
+        {!isMine && <Typography variant="body2">{username}</Typography>}
+        <Box display="flex">
+          <Box className="message__text-container" padding="12px" marginRight="4px">
+            <div className={`message__text-decoration ${isMine && 'message__text-decoration--hidden'}`}></div>
+            <Typography className="message__text">{children}</Typography>
+          </Box>
+          <Box alignSelf="flex-end">
+            <Typography variant="caption">{currentTime}</Typography>
+          </Box>
+        </Box>
+      </Box>  
     </Box>
   )
 }
