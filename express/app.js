98dd427faa023b3a88e1cde1d755343504df39c9
fatfirebird <fatfirebird@gmail.com>
2021-01-31 16:43:09 +0500

    edit docker compose config, add db connection

--- express/app.js
+++ express/app.js
@@ -1,12 +1,37 @@
 const express = require('express')
+const { Client } = require('pg')
 require('dotenv').config()
 const app = express()
 
 
 app.get('/', (req, res) => {
-  res.send('хуй соси быдло ебаное')
+  res.send('111')
 })
 
 app.listen(process.env.PORT, () => {
   console.log(`started on port: ${process.env.PORT}`)
 })
+
+console.log(process.env)
+const client = new Client({
+  user: process.env.POSTGRES_USER,
+  host: 'postgres',
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: 5432,
+  ssl: false
+})
+
+const connection = () => {
+  client.connect((error) => {
+    try {
+      console.error('db connected')
+    } catch {
+      console.error('not connected')
+      console.error(error)
+      connection()
+    }
+  })
+}
+
+connection()
