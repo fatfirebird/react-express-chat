bc189e780e87ff0f5a4a185420a229604b24ea09
fatfirebird <fatfirebird@gmail.com>
2021-04-19 15:17:52 +0500

    add storage persistans

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 import 'normalize.css'
 import './index.css'
 
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
-
--- frontend/src/pages/Auth/index.tsx
+++ frontend/src/pages/Auth/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Grid} from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 
 import AuthForm from '../../components/AuthForm'
 
 const Auth: React.FC = () => {
-  return(
-    <Grid className="container container--height" justify="center" alignItems="center" container spacing={6}>
+  return (
+    <Grid
+      className="container container--height"
+      justify="center"
+      alignItems="center"
+      container
+      spacing={6}
+    >
       <Grid item xs={12} sm={6} justify="center" alignItems="center">
         <AuthForm />
       </Grid>
